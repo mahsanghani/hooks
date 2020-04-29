619d5c6c0bd1be401a795aa1c3faec834e772ddc
mahsanghani <ghaniahsan@outlook.com>
2020-04-29 18:17:41 -0400

    added components

--- src/Playground.js
+++ src/Playground.js
@@ -1,26 +1,47 @@
 import React, { useState, useEffect, useRef } from 'react'
+import Name from './Name'
+import Canvas from './Canvas'
+import ColorPicker from './ColorPicker'
+import WindowSize from './WindowSize'
 import randomColor from 'randomcolor'
 
-export default function Playground() {
-    const [count, setCount] = useState(30)
+export default function Paint() {
+    const [colors, setColors] = useState([])
+    const [activeColor, setActiveColor] = useState(null)
+    const getColors = () => {
+        const baseColor = randomColor().slice(1);
+        fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
+            .then(res => res.json())
+            .then(res => {
+                setColors(res.colors.map(color => color.hex.value))
+                setActiveColor(res.colors[0].hex.value)
+            })
+    }
+    useEffect(getColors, [])
 
-    const inputRef = useRef()
-
-    const [color, setColor] = useState(null)
-    useEffect(() => {
-        setColor(randomColor())
-        inputRef.current.focus()
-    }, [count])
+    const headerRef = useRef({ offsetHeight: 0 })
 
     return (
-        <div style={{ borderTop: `10px solid ${color}`}}>
-            {count}
-            <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>
-            <button onClick={() => setCount(currentCount => currentCount + 1)}>+</button>
-            <hr />
-            <input ref={inputRef} type="range" onChange={e => setCount(e.target.value)} value={count} />
+        <div className="app">
+            <header ref={headerRef} style={{ borderTop: `10px solid ${activeColor}` }}>
+                <div>
+                    <Name />
+                </div>
+                <div style={{ marginTop: 10 }}>
+                    <ColorPicker
+                        colors={colors}
+                        activeColor={activeColor}
+                        setActiveColor={setActiveColor}
+                    />
+                </div>
+            </header>
+            {activeColor && (
+                <Canvas
+                    color={activeColor}
+                    height={window.innerHeight - headerRef.current.offsetHeight}
+                />
+            )}
+            <WindowSize />
         </div>
     )
 }
-
-// https://cdb.reacttraining.com/react-inline-functions-and-performance-bdff784f5578
